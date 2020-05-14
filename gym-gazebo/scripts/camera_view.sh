ee192faacaca1dc556032871c1c03669b2ed07a3
NachoAz <n.arranz.agueda@gmail.com>
2020-05-15 00:22:32 +0200

    Camera view scripted

--- gym-gazebo/scripts/camera_view.sh
+++ gym-gazebo/scripts/camera_view.sh
@@ -1,1 +1,14 @@
-rosrun image_view image_view image:=/F1ROS/cameraL/image_raw
+#!/bin/bash
+
+TOPIC=""
+
+if [ "$1" == "f1" ]; then
+    TOPIC="/F1ROS/cameraL/image_raw"
+elif [ "$1" == "turtlebot" ]; then
+    TOPIC="/camera/rgb/image_raw"
+else
+    printf "\n[ERROR] - No robot selected\n\n"
+    exit 1
+fi
+
+rosrun image_view image_view image:=$TOPIC
