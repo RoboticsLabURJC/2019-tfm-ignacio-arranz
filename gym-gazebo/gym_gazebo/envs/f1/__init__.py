9dc0c5b6933be34f056f35a5f768aa2743e4c486
NachoAz <n.arranz.agueda@gmail.com>
2020-06-03 23:33:56 +0200

    Fix error on qlearn environment

--- gym-gazebo/gym_gazebo/envs/f1/__init__.py
+++ gym-gazebo/gym_gazebo/envs/f1/__init__.py
@@ -1,2 +1,2 @@
 from gym_gazebo.envs.f1.env_gazebo_f1_camera import GazeboF1CameraEnv
-from gym_gazebo.envs.f1.env_gazebo_f1_qlearn import GazeboF1QLearnEnv
+from gym_gazebo.envs.f1.env_gazebo_f1_laser_qlearn import GazeboF1QLearnEnv
