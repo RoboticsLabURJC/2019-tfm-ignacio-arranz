370a5fd2df49a3a50cb399bb6ac59769eb23719e
NachoAz <n.arranz.agueda@gmail.com>
2020-06-21 00:55:20 +0200

    menu added to helper

--- gym-gazebo/scripts/camera_view.sh
+++ gym-gazebo/scripts/camera_view.sh
@@ -7,7 +7,7 @@ if [ "$1" == "f1" ]; then
 elif [ "$1" == "turtlebot" ]; then
     TOPIC="/camera/rgb/image_raw"
 else
-    printf "\n[ERROR] - No robot selected\n\n"
+    printf "\n[ERROR] - No robot selected (use: 'f1' or 'turtlebot')\n\n"
     exit 1
 fi
 
