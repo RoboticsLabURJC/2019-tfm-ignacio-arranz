0f21d2a787a4ba27a563f05188b249f2e1c11b71
NachoAz <n.arranz.agueda@gmail.com>
2020-06-27 17:19:11 +0200

    refactor

--- gym-gazebo/gym_gazebo/envs/f1/__init__.py
+++ gym-gazebo/gym_gazebo/envs/f1/__init__.py
@@ -1,5 +1,5 @@
-from gym_gazebo.envs.f1.env_gazebo_f1_camera_qlearn import GazeboF1QlearnCameraEnv
-from gym_gazebo.envs.f1.env_gazebo_f1_laser_qlearn import GazeboF1QlearnLaserEnv
+from gym_gazebo.envs.f1.env_gazebo_f1_qlearn_camera   import GazeboF1QlearnCameraEnv
+from gym_gazebo.envs.f1.env_gazebo_f1_qlearn_laser import GazeboF1QlearnLaserEnv
 
 # DQN
-from gym_gazebo.envs.f1.env_gazebo_f1_camera_dqn import GazeboF1CameraEnvDQN
+from gym_gazebo.envs.f1.env_gazebo_f1_dqn_camera import GazeboF1CameraEnvDQN
