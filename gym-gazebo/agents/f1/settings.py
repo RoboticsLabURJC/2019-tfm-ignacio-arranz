6d1267c9746c33e212d6de2dd752831cc40825d4
NachoAz <n.arranz.agueda@gmail.com>
2020-07-04 20:47:11 +0200

    More variables into seetings file

--- gym-gazebo/agents/f1/settings.py
+++ gym-gazebo/agents/f1/settings.py
@@ -1,6 +1,9 @@
 # Global variables
 
+import numpy as np
+
 from environs import Env
+
 
 env = Env()
 env.read_env()
@@ -12,6 +15,9 @@ save_model = env.bool("SAVE_MODEL", False)
 load_model = env.bool("LOAD_MODEL", False)
 
 # === ACTIONS SET ===
+# Deprecated?
+space_reward = np.flip(np.linspace(0, 1, 300))
+
 # action: (lineal, angular)
 actions_simple = {
     0: (3, 0),
@@ -39,9 +45,21 @@ actions_hard = {
     7: (2, -5),
 }
 
-# POSES
-gazebo_positions = [(0, 53.462, -41.988, 0.004, 0, 0, 1.57, -1.57),
-             (1, 53.462, -8.734, 0.004, 0, 0, 1.57, -1.57),
-             (2, 39.712, -30.741, 0.004, 0, 0, 1.56, 1.56),
-             (3, -7.894, -39.051, 0.004, 0, 0.01, -2.021, 2.021),
-             (4, 20.043, 37.130, 0.003, 0, 0.103, -1.4383, -1.4383)]
+# === POSES ===
+gazebo_positions = [(0, 53.462, -41.988, 0.004, 0, 0,      1.57,   -1.57),
+                    (1, 53.462, -8.734,  0.004, 0, 0,      1.57,   -1.57),
+                    (2, 39.712, -30.741, 0.004, 0, 0,      1.56,    1.56),
+                    (3, -6.861, -36.481, 0.004, 0, 0.01,   -0.858, 0.613),
+                    (4, 20.043,  37.130, 0.003, 0, 0.103, -1.4383, -1.4383)]
+
+# === CAMERA ===
+# Images size
+witdh = 640
+center_image = witdh/2
+
+# Coord X ROW
+x_row = [260, 360, 450]
+# Maximum distance from the line
+ranges = [300, 280, 250]  # Line 1, 2 and 3
+reset_range = [-40, 40]
+last_center_line = 0
