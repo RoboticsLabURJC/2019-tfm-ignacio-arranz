5aadba5706fa123296cd77f0791fa554a8bf1dfa
NachoAz <n.arranz.agueda@gmail.com>
2020-07-19 23:30:15 +0200

    no gui, back to original pose with camera

--- gym-gazebo/agents/f1/f1_follow_line_qlearn.py
+++ gym-gazebo/agents/f1/f1_follow_line_qlearn.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     last_time_steps = np.ndarray(0)
 
     actions = range(env.action_space.n)
-    print("ACTIONSSSSS: {}".format(actions))
+
     qlearn = QLearn(actions=actions, alpha=0.2, gamma=0.9, epsilon=0.99)
 
     if settings.load_model:
