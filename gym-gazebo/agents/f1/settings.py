553aa7e048a862b36085f2a221ea83aeddec3e7c
NachoAz <n.arranz.agueda@gmail.com>
2020-08-26 23:23:49 +0200

    reordered settings

--- gym-gazebo/agents/f1/settings.py
+++ gym-gazebo/agents/f1/settings.py
@@ -2,7 +2,6 @@
 # Global variables file
 ###########################
 import numpy as np
-import json
 
 
 # === BASIC CONFIG ===
@@ -15,47 +14,6 @@ save_model = True
 load_model = False
 
 
-# === GAZEBO POSITIONS ===
-simple_gazebo_positions = [(0, 53.462, -41.988, 0.004, 0, 0, 1.57, -1.57),
-                           (1, 53.462, -8.734,  0.004, 0, 0, 1.57, -1.57),
-                           (2, 39.712, -30.741, 0.004, 0, 0, 1.56, 1.56),
-                           (3, -6.861,  -36.481, 0.004, 0, 0.01, -0.858, 0.613),
-                           (4, 20.043, 37.130, 0.003, 0, 0.103, -1.4383, -1.4383)]
-
-nurburgring_gazebo_positions = [(0, -23.0937, -2.9703, 0, 0.0050, 0.0013, -0.9628, 0.2699),
-                                (1, -32.3188, 12.2921, 0, 0.0014, 0.0049, -0.2727, 0.9620),
-                                (2, -17.4155, -24.1243, 0, 0.0001, 0.0051, -0.0192, 1),
-                                (3, 31.3967, -4.6166, 0, 0.0030, 0.0041, 0.6011, 0.7991),
-                                (4, -56.1261, 4.1047, 0, 0.0043, -0.0027, -0.8517, -0.5240)]
-
-# === CIRCUIT ===
-envs_params = {
-    "simple": {
-        "env": "GazeboF1QlearnCameraEnv-v0",
-        "launch": "F1Cameracircuit_v0.launch",
-        "gaz_pos": simple_gazebo_positions,
-        "sensor": "camera"
-    },
-    "nurburgring": {
-        "env": "GazeboF1QlearnCameraEnv-v0",
-        "launch": "f1_1_nurburgrinlineROS.launch",
-        "gaz_pos": nurburgring_gazebo_positions,
-        "sensor": "camera"
-    },
-    "lemans": {
-        "env": "GazeboF1QlearnCameraEnv-v0",
-        "launch": "f1_lemans_camera.launch",
-        "gaz_pos": "",
-        "sensor": "camera"
-    },
-    "simple_laser": {
-        "env": "GazeboF1QlearnLaserEnv-v0",
-        "launch": "F1Lasercircuit_v0.launch",
-        "gaz_pos": "",
-        "sensor": "laser"
-    }
-}
-
 # === PARAMS ===
 algorithm_params = {"alpha": 0.2,
                     "gamma": 0.9,
@@ -63,9 +21,9 @@ algorithm_params = {"alpha": 0.2,
                     "highest_reward": 4000
                     }
 
+
 # === ACTIONS SET ===
-actions_set = "medium"  # test, simple, medium, hard
-space_reward = np.flip(np.linspace(0, 1, 300))  # Deprecated
+actions_set = "simple"  # test, simple, medium, hard
 
 # action: (lineal, angular)
 if actions_set == "simple":
@@ -99,6 +57,52 @@ elif actions_set == "test":
         2: (0, 0),
     }
 
+
+# === GAZEBO POSITIONS ===
+simple_gazebo_positions = [(0, 53.462, -41.988, 0.004, 0, 0, 1.57, -1.57),
+                           (1, 53.462, -8.734,  0.004, 0, 0, 1.57, -1.57),
+                           (2, 39.712, -30.741, 0.004, 0, 0, 1.56, 1.56),
+                           (3, -6.861,  -36.481, 0.004, 0, 0.01, -0.858, 0.613),
+                           (4, 20.043, 37.130, 0.003, 0, 0.103, -1.4383, -1.4383)]
+
+nurburgring_gazebo_positions = [(0, -23.0937, -2.9703, 0, 0.0050, 0.0013, -0.9628, 0.2699),
+                                (1, -32.3188, 12.2921, 0, 0.0014, 0.0049, -0.2727, 0.9620),
+                                (2, -17.4155, -24.1243, 0, 0.0001, 0.0051, -0.0192, 1),
+                                (3, 31.3967, -4.6166, 0, 0.0030, 0.0041, 0.6011, 0.7991),
+                                (4, -56.1261, 4.1047, 0, 0.0043, -0.0027, -0.8517, -0.5240)]
+
+
+# === CIRCUIT ===
+envs_params = {
+    "simple": {
+        "circuit_name": "simple",
+        "env": "GazeboF1QlearnCameraEnv-v0",
+        "launch": "F1Cameracircuit_v0.launch",
+        "gaz_pos": simple_gazebo_positions,
+        "sensor": "camera"
+    },
+    "nurburgring": {
+        "circuit_name": "nurburgring",
+        "env": "GazeboF1QlearnCameraEnv-v0",
+        "launch": "f1_1_nurburgrinlineROS.launch",
+        "gaz_pos": nurburgring_gazebo_positions,
+        "sensor": "camera"
+    },
+    "lemans": {
+        "circuit_name": "lemans",
+        "env": "GazeboF1QlearnCameraEnv-v0",
+        "launch": "f1_lemans_camera.launch",
+        "gaz_pos": "",
+        "sensor": "camera"
+    },
+    "simple_laser": {
+        "circuit_name": "simple_laser",
+        "env": "GazeboF1QlearnLaserEnv-v0",
+        "launch": "F1Lasercircuit_v0.launch",
+        "gaz_pos": "",
+        "sensor": "laser"
+    }
+}
 
 
 # === CAMERA ===
